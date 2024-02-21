0212fba65907677fa37361c22976ae13b0df71aa
Alejandra Colcha <da.colcha@hotmail.com>
2024-02-21 17:16:48 -0500

    Prueba 21022024

--- src/main/java/com/saew/controllers/StudentsController.java
+++ src/main/java/com/saew/controllers/StudentsController.java
@@ -1,10 +1,16 @@
 package com.saew.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,43 +19,75 @@ import com.saew.dto.Student;
 @RestController
 @RequestMapping("/students")
 public class StudentsController {
-    
-    
-    public List<Student> getAllStudents() {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();
-    }
-    
 
-    
-    public List<Student> getStudentByName(String name) {    
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();
+    Student student1 = new Student(1, "Alejandra Colcha", "dacol@email.com", 8.56f);
+    Student student2 = new Student(2, "Carlos Perez", "carlos@email.com", 9.56f);
+    Student student3 = new Student(3, "Melisa Cobos", "melisa@email.com", 8.80f);
+
+    List<Student> studentsList = new ArrayList<Student>() {
+        {
+            add(student1);
+            add(student2);
+            add(student3);
+        }
+    };
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentsList;
     }
-       
-    public ResponseEntity<Student> getStudentById(String id) {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();
+
+    @GetMapping("/{name}")
+    public List<Student> getStudentByName(@PathVariable String name) {
+        return studentsList.stream().filter(student -> student.getNombre().toLowerCase().contains(name)).toList();
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable String id) {
+        Optional<Student> student = studentsList.stream().filter(s -> s.getId() == (Integer.parseInt(id))).findFirst();
+
+        if (student.isPresent()) {
+            return ResponseEntity.ok(student.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Student> createStudent(Student student) {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        studentsList.add(student);
+        return ResponseEntity.ok(student);
     }
 
     @PatchMapping
-    public ResponseEntity<Student> updateProduct(Student product) {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();   
-    }
-    
-    public ResponseEntity<Student> deleteStudent(String id) {
-        //TODO: Implement the method
-        throw new UnsupportedOperationException();   
+    public ResponseEntity<Student> updateProduct(@RequestBody Student student) {
+        boolean studentExist = studentsList.stream()
+                .filter(s -> s.getId() == student.getId())
+                .findFirst().isPresent();
+        if (studentExist) {
+            studentsList.remove(studentsList.stream()
+                    .filter(s -> s.getId() == student.getId())
+                    .findFirst().get());
+
+            studentsList.add(student);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable String id) {
+        boolean studentExist = studentsList.stream()
+                .filter(s -> s.getId() == (Integer.parseInt(id)))
+                .findFirst().isPresent();
+        if (studentExist) {
+            studentsList
+                    .remove(studentsList.stream().filter(s -> s.getId() == (Integer.parseInt(id))).findFirst().get());
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-    
 }
--- src/main/java/com/saew/dto/Student.java
+++ src/main/java/com/saew/dto/Student.java
@@ -1,5 +1,53 @@
 package com.saew.dto;
 
 public class Student {
-    //TODO: Add the missing fields
+
+    private Integer id;
+    private String nombre;
+    private String correo;
+    private Float gpa;
+
+    public Student(Integer id, String nombre, String correo, Float gpa) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.gpa = gpa;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(Float gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student id=" + id + ", nombre=" + nombre + ", correo=" + correo + ", gpa=" + gpa;
+    }
 }
